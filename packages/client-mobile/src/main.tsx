c00330f42aa6f82c1c616809608ffe0fe3b1af06
SnlperStripes <fabiogr@gmx.de>
2024-04-26 11:50:06 +0200

    chore: add login page

--- packages/client-mobile/src/main.tsx
+++ packages/client-mobile/src/main.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
+import LoginPage from "$components/login/login";
+import Button from "$components/button/button";
 
 // CSS
 import "./index.css";
 
+
 const root = document.getElementById("root")!;
 
-createRoot(root).render(
-  <React.StrictMode>
-    {/* TODO */}
-  </React.StrictMode>
-);
+const App: React.FC = () => {
+  const [showLoginPage, setShowLoginPage] = useState(false);
+
+  const showLoginPageHandler = () => {
+    setShowLoginPage(true);
+  };
+
+  return (
+    <React.StrictMode>
+      {showLoginPage && <LoginPage />}
+      {!showLoginPage && (
+        <div className="flex justify-center mt-4">
+          <Button onClick={showLoginPageHandler} text="Login" />
+        </div>
+      )}
+    </React.StrictMode>
+  );
+};
+
+createRoot(root).render(<App />);
