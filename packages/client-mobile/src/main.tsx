efba474bbad8ee294af5efc89361855f66cb80f1
SnlperStripes <fabiogr@gmx.de>
2024-04-26 15:34:13 +0200

    chore: improve main.tsx

--- packages/client-mobile/src/main.tsx
+++ packages/client-mobile/src/main.tsx
@@ -2,32 +2,28 @@ import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import LoginPage from "$components/pages/login";
 import Button from "$components/button/button";
-
-// CSS
-import "./index.css";
 import Header from "$components/header/header";
-
-
-const root = document.getElementById("root")!;
+import "./index.css";
 
 const App: React.FC = () => {
   const [showLoginPage, setShowLoginPage] = useState(false);
 
-  const showLoginPageHandler = () => {
+  const handleLoginClick = () => {
     setShowLoginPage(true);
   };
 
   return (
     <React.StrictMode>
       <Header />
-      {showLoginPage && <LoginPage />}
       {!showLoginPage && (
         <div className="flex justify-center mt-4">
-          <Button onClick={showLoginPageHandler} text="Login" />
+          <Button onClick={handleLoginClick} text="Login" />
         </div>
       )}
+      {showLoginPage && <LoginPage />}
     </React.StrictMode>
   );
 };
 
+const root = document.getElementById("root")!;
 createRoot(root).render(<App />);
