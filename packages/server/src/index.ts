fedca044f27692a0eb135ee4807113d5c7581293
wgumenyuk <wlad@gumenyuk.de>
2024-04-26 11:16:53 +0200

    feat: connect to MongoDB on start-up

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,12 +2,16 @@ import Koa from "koa";
 
 // Intern
 import { log } from "$internal/logger";
+import { loadMongoDb } from "$internal/mongodb";
 import { router } from "$api/v1";
 
 /**
   Port, auf dem die API läuft.
 */
 const PORT = process.env.PORT || 3000;
+
+// Mit MongoDB verbinden.
+await loadMongoDb();
 
 const app = new Koa();
 
