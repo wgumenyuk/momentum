75856e71739178711635ac1e03b853cf8a616416
wgumenyuk <wlad@gumenyuk.de>
2024-05-07 13:41:31 +0200

    feat: add shared api types

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,55 @@
 /**
-  `Momentum`-Namespace für geteilte Typdefintionen.
+  API-Antwortcode.
 */
-export namespace Momentum {};
+export enum ResponseCode {
+  Success,
+  InvalidData
+};
+
+/**
+  API-Antwort.
+*/
+export type Response<
+  Data extends Record<string, never>,
+  Error extends Record<string, never>
+> = {
+  /**
+    Ob die Anfrage erfolgreich war.
+  */
+  success: true;
+
+  /**
+    HTTP-Status.
+  */
+  status: number;
+
+  /**
+    API-Antwortcode.
+  */
+  code: ResponseCode;
+
+  /**
+    Mitgelieferte Daten.
+  */
+  data: Data;
+} | {
+  /**
+    Ob die Anfrage erfolgreich war.
+  */
+  success: false;
+
+  /**
+    HTTP-Status.
+  */
+  status: number;
+
+  /**
+    API-Antwortcode.
+  */
+  code: ResponseCode;
+
+  /**
+    Mitgelieferte Details über den Fehler.
+  */
+  details: Error;
+};
