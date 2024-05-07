a57efec0eef49a1f5fdb201c8a4cb8e5f6fd7eb6
wgumenyuk <wlad@gumenyuk.de>
2024-05-07 13:10:05 +0200

    feat: add koa server entrypoint

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hallo Welt!");
+import Koa from "koa";
+import cors from "@koa/cors";
+
+// Intern
+import { log } from "$internal/logger";
+
+const {
+  PORT = 3000,
+  ORIGIN = "*"
+} = process.env;
+
+const app = new Koa();
+
+app.use(
+  cors({
+    origin: ORIGIN
+  })
+);
+
+app.listen(PORT, () => {
+  log.info(`server listening on port ${PORT}`);
+});
