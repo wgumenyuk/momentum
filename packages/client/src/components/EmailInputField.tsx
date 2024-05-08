f5a53d5cd62cc63b82c56868c3d9b1c51f5c1643
SnlperStripes <fabiogr@gmx.de>
2024-05-08 13:21:42 +0200

    feat: add title class to PasswordInputField chore: reformat the code of the InputComponents

--- packages/client/src/components/EmailInputField.tsx
+++ packages/client/src/components/EmailInputField.tsx
@@ -8,16 +8,21 @@ interface EmailInputFieldProps {
   onChange: (value: string) => void;
 }
 
-const EmailInputField: React.FC<EmailInputFieldProps> = ({ placeholder, value, onChange }) => {
-  return (
-    <div className="flex flex-col items-start rounded-lg shadow-sm p-1 w-full h-auto bg-white">
-      <span className="text-xs text-gray-600 mb-1 bg-transparent" style={{ backgroundColor: "transparent", padding: 0, margin: 0, border: "none" }}>E-Mail</span>
-      <div className="flex items-center w-full">
-        <Mail className="text-gray-900 mr-1 text-lg" />
-        <InputField placeholder={placeholder} value={value} onChange={onChange} className="w-full shadow-none rounded-none bg-transparent text-sm" /> 
-      </div>
+const EmailInputField: React.FC<EmailInputFieldProps> = ({ placeholder, value, onChange }) => (
+  <div className="flex flex-col items-start rounded-lg shadow-sm p-1 w-full h-auto bg-white">
+    <span className="text-xs text-gray-600 mb-1 bg-transparent" style={{ backgroundColor: "transparent", padding: 0, margin: 0, border: "none" }}>
+      E-Mail
+    </span>
+    <div className="flex items-center w-full">
+      <Mail className="text-gray-900 mr-1 text-lg" />
+      <InputField 
+        placeholder={placeholder} 
+        value={value} 
+        onChange={onChange} 
+        className="w-full shadow-none rounded-none bg-transparent text-sm" 
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default EmailInputField;
