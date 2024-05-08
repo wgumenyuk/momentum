3d6a6da477a13c76080d991d6a03090a2f6c2b38
SnlperStripes <fabiogr@gmx.de>
2024-05-08 17:03:41 +0200

    feat: add component: "Remember Me" TickBox

---
+++ packages/client/src/components/TickBoxes/RememberMeTickBox.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import TickBox from "./TickBox";
+
+const RememberMeCheckbox = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = (checked: boolean) => {
+    setIsChecked(checked);
+  };
+
+  return (
+    <TickBox 
+      label="Remember Me" 
+      checked={isChecked} 
+      onChange={handleCheckboxChange}
+    />
+  );
+};
+
+export default RememberMeCheckbox;
