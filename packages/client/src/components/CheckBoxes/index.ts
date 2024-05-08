89b2e8f5ad1c220c129839ed2b860d1af893f4d0
SnlperStripes <fabiogr@gmx.de>
2024-05-08 18:58:18 +0200

    feat: add index.ts to the CheckBoxes component folder

---
+++ packages/client/src/components/CheckBoxes/index.ts
@@ -0,0 +1,4 @@
+export { default as CheckBox } from "./CheckBox";
+export { default as RememberMeCheckbox } from "./RememberMeCheckBox";
+export { default as SubscribeToNewsletterCheckbox } from "./SubscribeToNewsletterCheckBox";
+export { default as TermsAndConditionsCheckbox } from "./TermsAndConditionsCheckBox";
