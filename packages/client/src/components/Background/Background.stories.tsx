27faa7c0a99bbfddadd449714806ca9af901d10e
SnlperStripes <fabiogr@gmx.de>
2024-05-09 21:04:12 +0200

    feat: create background layout component

---
+++ packages/client/src/components/Background/Background.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import BackgroundLayout from "./BackgroundLayout";
+
+export default {
+    title: "Components/BackgroundLayout",
+    component: BackgroundLayout
+} as Meta;
+
+// You can directly use the component and embed the content since there are no args to manage
+export const Default: StoryFn = () => (
+    <BackgroundLayout>
+        <div style={{ padding: "20px" }}>This is a sample content inside the BackgroundLayout.</div>
+    </BackgroundLayout>
+);
