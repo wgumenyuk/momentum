6ec87cbb75fcb77716ab3951879f959faa0c3b5e
SnlperStripes <fabiogr@gmx.de>
2024-05-10 12:52:17 +0200

    feat: reduce button size for small screens

--- packages/client/src/components/Buttons/BigButtonBlue.tsx
+++ packages/client/src/components/Buttons/BigButtonBlue.tsx
@@ -9,7 +9,7 @@ const BigButtonBlue: React.FC<BigButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-gradient-btn text-white py-2 px-20 rounded-lg shadow-md hover:bg-gradient-btn focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-opacity-50 transition duration-150 ease-in-out text-lg"
+      className="bg-gradient-btn text-white py-2 px-4 sm:px-6 md:px-10 lg:px-20 rounded-lg shadow-md hover:bg-gradient-btn focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-opacity-50 transition duration-150 ease-in-out text-lg"
     >
       {text}
     </button>
