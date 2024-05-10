4ef2618a6065d727f781458b44c98891d9e039b1
SnlperStripes <fabiogr@gmx.de>
2024-05-10 11:35:47 +0200

    feat: add RegisterPage feat: add pages.stories

---
+++ packages/client/src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BackgroundLayout } from "$components/Background";
+
+const RegisterPage: React.FC = () => {
+  return (
+    <BackgroundLayout>
+      <h1>Register</h1>
+    </BackgroundLayout>
+  );
+};
+
+export default RegisterPage;
