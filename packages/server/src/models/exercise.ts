40ab418300f185f6b181975c4c5302040d78f965
wgumenyuk <wlad@gumenyuk.de>
2024-05-13 13:12:43 +0200

    feat: add `Exercise` model

---
+++ packages/server/src/models/exercise.ts
@@ -0,0 +1,56 @@
+import { Schema, model } from "mongoose";
+
+/**
+  Übung.
+*/
+type Exercise = {
+  /**
+    Übung.
+  */
+  id: string;
+
+  /**
+    Betroffene Muskeln.
+  */
+  muscles: string[];
+
+  /**
+    Benötige Ausrüstung.
+  */
+  equipment: string[];
+
+  /**
+    Schwierigkeitsgrad.
+  */
+  difficulty: "easy" | "intermediate" | "advanced",
+};
+
+const ExerciseSchema = new Schema<Exercise>({
+  id: {
+    type: "string",
+    required: true,
+    unique: true
+  },
+  muscles: {
+    type: [ String ],
+    required: true
+  },
+  equipment: {
+    type: [ String ],
+    required: true
+  },
+  difficulty: {
+    type: "string",
+    required: true,
+    enum: [
+      "easy",
+      "intermediate",
+      "advanced"
+    ]
+  }
+});
+
+/**
+  `Exercise`-Modell.
+*/
+export const Exercise = model("Exercise", ExerciseSchema);
