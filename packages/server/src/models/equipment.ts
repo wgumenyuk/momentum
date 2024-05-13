9375005f23b2b5fdf45f617e5f1ea4d8295d2ba7
wgumenyuk <wlad@gumenyuk.de>
2024-05-13 16:47:56 +0200

    feat: add `Equipment` model

---
+++ packages/server/src/models/equipment.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+/**
+  Ausrüstung.
+*/
+type Equipment = {
+  /**
+    ID.
+  */
+  id: string;
+
+  /**
+    Liste von Übungen, die mit dieser Ausrüstung ausgeführt werden können.
+  */
+  exercises: string[];
+};
+
+const EquipmentSchema = new Schema<Equipment>({
+  id: {
+    type: "string",
+    required: true,
+    unique: true
+  },
+  exercises: {
+    type: [ String ],
+    required: true
+  }
+});
+
+/**
+  `Equipment`-Modell.
+*/
+export const Equipment = model<Equipment>("Equipment", EquipmentSchema);
