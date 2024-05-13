437271c9635d4488c80d27bf8d014d73a4fe06a2
SnlperStripes <fabiogr@gmx.de>
2024-05-13 18:07:43 +0200

    feat: add login service

--- packages/server/src/services/auth.ts
+++ packages/server/src/services/auth.ts
@@ -1,7 +1,8 @@
-import { RegisterSchema, StatusCode, ErrorCode } from "@momentum/shared";
+import { RegisterSchema, StatusCode, ErrorCode, LoginSchema } from "@momentum/shared";
 import { ok, nok } from "$api/response";
-import { hashPassword } from "$services/crypto";
+import { hashPassword, verifyPassword } from "$services/crypto";
 import { User } from "$models/user";
+import jwt from "jwt-promisify";
 
 // Types
 import type { Context } from "koa";
@@ -13,7 +14,7 @@ import type { ErrorCodeValue } from "@momentum/shared";
 export const register = async (ctx: Context) => {
   const { success, error, data } = RegisterSchema.safeParse(ctx.request.body);
 
-  if(!success) {
+  if (!success) {
     return nok(
       ctx,
       StatusCode.BadRequest,
@@ -29,7 +30,7 @@ export const register = async (ctx: Context) => {
     })
   );
 
-  if(isEmailTaken) {
+  if (isEmailTaken) {
     return nok(ctx, StatusCode.BadRequest, ErrorCode.RegisterEmailTaken);
   }
 
@@ -44,5 +45,51 @@ export const register = async (ctx: Context) => {
 
   ok(ctx, StatusCode.Success, {
     id: user.id
+  });
+};
+
+/**
+  Authentifiziert einen Benutzer und gibt ein JWT aus.
+*/
+export const login = async (ctx: Context) => {
+  const { success, error, data } = LoginSchema.safeParse(ctx.request.body);
+
+  if (!success) {
+    return nok(
+      ctx,
+      StatusCode.BadRequest,
+      error.issues[0].message as ErrorCodeValue
+    );
+  }
+
+  const { email, password } = data;
+
+  const user = await User.findOne({
+    email
+  });
+
+  if (!user) {
+    return nok(ctx, StatusCode.Unauthenticated, ErrorCode.LoginInvalidEmail);
+  }
+
+  const isPasswordCorrect = await verifyPassword(password, user.password);
+
+  if (!isPasswordCorrect) {
+    return nok(ctx, StatusCode.Unauthenticated, ErrorCode.LoginInvalidPassword);
+  }
+
+  // Generate JWT
+  const token = await jwt.sign(
+    {
+      id: user.id,
+      email: user.email
+    },
+    process.env.JWT_SECRET, // Ensure your JWT secret is securely stored and accessed
+    { expiresIn: "1h" } // Token expires in one hour
+  );
+
+  // Authentication successful, return the token
+  ok(ctx, StatusCode.Success, {
+    token: token
   });
 };
