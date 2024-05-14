8930c9bff654697ea711a5090f6f0561509b3ce1
SnlperStripes <fabiogr@gmx.de>
2024-05-14 19:35:36 +0200

    feat: create workout page feat: create modifyWorkout page

---
+++ packages/client/src/pages/workout/ModifyWorkout.tsx
@@ -0,0 +1,19 @@
+import { BackgroundLayout } from "$components/Background";
+import NavigationBar from "$components/navigation/NavigationBar";
+import React from "react";
+
+const ModifyWorkout: React.FC = () => {
+  return (
+    <>
+      <BackgroundLayout>
+        <div className="flex flex-col items-center space-y-4">
+          <h1 className="text-2xl font-bold text-black">Modify Workout</h1>
+          <p className="text-lg text-black">Here you can modify your workout details.</p>
+        </div>
+      </BackgroundLayout>
+      <NavigationBar />
+    </>
+  );
+};
+
+export default ModifyWorkout;
