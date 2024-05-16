c62249f5bada72a9d095259a1167fc90c0fcb8e6
SnlperStripes <fabiogr@gmx.de>
2024-05-16 14:02:38 +0200

    feat: remove reduncancy (isAuthenticated) in workout route

--- packages/server/src/api/v1/workout.ts
+++ packages/server/src/api/v1/workout.ts
@@ -12,22 +12,24 @@ export const workoutRouter = new Router({
   prefix: "/users/:uid/workouts"
 });
 
-workoutRouter.post("/", isAuthenticated, async (ctx) => {
+workoutRouter.use(isAuthenticated);
+
+workoutRouter.post("/", async (ctx) => {
   await createWorkout(ctx);
 });
 
-workoutRouter.get("/:id", isAuthenticated, async (ctx) => {
+workoutRouter.get("/:id", async (ctx) => {
   await getWorkout(ctx);
 });
 
-workoutRouter.get("/", isAuthenticated, async (ctx) => {
+workoutRouter.get("/", async (ctx) => {
   await getWorkouts(ctx);
 });
 
-workoutRouter.put("/:id", isAuthenticated, async (ctx) => {
+workoutRouter.put("/:id", async (ctx) => {
   await updateWorkout(ctx);
 });
 
-workoutRouter.delete("/:id", isAuthenticated, async (ctx) => {
+workoutRouter.delete("/:id", async (ctx) => {
   await deleteWorkout(ctx);
 });
