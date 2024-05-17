92c7d8a3a4e4ea41bcec3abd00524af60aca2604
wgumenyuk <wlad@gumenyuk.de>
2024-05-17 18:14:22 +0200

    chore: add `jsx-tag-spacing` rule

--- packages/client/eslint.config.js
+++ packages/client/eslint.config.js
@@ -1,5 +1,15 @@
 import momentum from "@momentum/eslint-config";
 
 export default [
-  ...momentum
+  ...momentum,
+  {
+    rules: {
+      "@stylistic/jsx-tag-spacing": [
+        "error",
+        {
+          "beforeSelfClosing": "never"
+        }
+      ]
+    }
+  }
 ];
