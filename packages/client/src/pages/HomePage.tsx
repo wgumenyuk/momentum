f82a5ae6f1152334ec858d7c3cab4dfa6cf5ee34
SnlperStripes <fabiogr@gmx.de>
2024-05-17 14:43:34 +0200

    feat: add HomePage

---
+++ packages/client/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to Momentum!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
