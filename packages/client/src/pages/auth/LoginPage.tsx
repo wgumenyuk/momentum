170a4fb4c3ae9bac5b15293587a274b951909d81
SnlperStripes <fabiogr@gmx.de>
2024-05-18 14:14:11 +0200

    refactor: create variants of checkbox, reduce component amount to one

--- packages/client/src/pages/auth/LoginPage.tsx
+++ packages/client/src/pages/auth/LoginPage.tsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { BackgroundLayout } from "$components/Background";
 import { Auth } from "$internal/api";
-import { InputField } from "$components/InputFields";
+import InputField from "$components/InputFields/InputField";
 import BigButton from "$components/Buttons/BigButton";
+import CheckBox from "$components/CheckBoxes/CheckBox";
 
 const LoginPage: React.FC = () => {
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("");
+  const [ rememberMe, setRememberMe ] = useState(false);
 
   const navigate = useNavigate();
 
@@ -33,7 +35,7 @@ const LoginPage: React.FC = () => {
 
     localStorage.setItem("token", token);
     navigate("/home");
-  }; 
+  };
 
   return (
     <BackgroundLayout>
@@ -42,23 +44,45 @@ const LoginPage: React.FC = () => {
           Sign in to <span className="text-blue-300">Momentum</span>
         </h1>
         <form className="space-y-4" onSubmit={(e) => e.preventDefault()}>
-          <InputField placeholder="you@example.com" value={email} onChange={setEmail} variant="email" title="E-Mail"/>
-          <InputField placeholder="********" value={password} onChange={setPassword} variant="password" title="Password"/>
-          <div className="mt-4">
-            <label className="flex items-center">
-              <input type="checkbox" className="form-checkbox"/>
-              <span className="ml-2 text-sm">Remember me</span>
-            </label>
-          </div>
+          <InputField
+            placeholder="you@example.com"
+            value={email}
+            onChange={setEmail}
+            variant="email"
+            title="E-Mail"
+          />
+          <InputField
+            placeholder="********"
+            value={password}
+            onChange={setPassword}
+            variant="password"
+            title="Password"
+          />
+          <CheckBox
+            variant="rememberMe"
+            checked={rememberMe}
+            onChange={setRememberMe}
+          />
           <div className="flex justify-center mt-6">
             <BigButton text="Sign In" onClick={handleSubmit} variant="blue"/>
           </div>
           <div className="text-center mt-2">
-            <Link to="/forgot-password" className="text-sm text-blue-300 hover:text-blue-600">Forgot password?</Link>
+            <Link
+              to="/forgot-password"
+              className="text-sm text-blue-300 hover:text-blue-600"
+            >
+              Forgot password?
+            </Link>
           </div>
         </form>
         <div className="text-center mt-6 text-sm">
-          Don’t have an account? <Link to="/register" className="text-blue-300 hover:text-blue-600">Sign up.</Link>
+          Don’t have an account?{" "}
+          <Link
+            to="/register"
+            className="text-blue-300 hover:text-blue-600"
+          >
+            Sign up.
+          </Link>
         </div>
       </div>
     </BackgroundLayout>
