8316f4a40698a1a719bd591fafafc74fae8be176
SnlperStripes <fabiogr@gmx.de>
2024-05-23 16:49:02 +0200

    fix: bugfix in index.ts for checkboxes

--- packages/client/src/components/CheckBoxes/index.ts
+++ packages/client/src/components/CheckBoxes/index.ts
@@ -1,4 +1,1 @@
 export { default as CheckBox } from "./CheckBox";
-export { default as RememberMeCheckbox } from "./RememberMeCheckBox";
-export { default as SubscribeToNewsletterCheckbox } from "./SubscribeToNewsletterCheckBox";
-export { default as TermsAndConditionsCheckbox } from "./TermsAndConditionsCheckBox";
