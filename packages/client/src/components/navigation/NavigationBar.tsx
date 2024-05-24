04040406a3a007454806937d6494dc7aeadd12fe
SnlperStripes <fabiogr@gmx.de>
2024-05-24 09:59:31 +0200

    feat: add links to navigation bar tabs

--- packages/client/src/components/navigation/NavigationBar.tsx
+++ packages/client/src/components/navigation/NavigationBar.tsx
@@ -1,26 +1,27 @@
 // NavigationBar.tsx
 import React from "react";
+import { Link } from "react-router-dom";
 import { Home, Dumbbell, Users, User } from "lucide-react";
 
 const NavigationBar = () => {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-dark-gray p-4 flex justify-around">
-      <button className="text-cool-gray flex flex-col items-center">
+      <Link to="/" className="text-cool-gray flex flex-col items-center">
         <Home className="w-6 h-6"/>
         <span className="text-xs mt-1">Home</span>
-      </button>
-      <button className="text-cool-gray flex flex-col items-center">
+      </Link>
+      <Link to="/workouts" className="text-cool-gray flex flex-col items-center">
         <Dumbbell className="w-6 h-6"/>
         <span className="text-xs mt-1">Workouts</span>
-      </button>
-      <button className="text-cool-gray flex flex-col items-center">
+      </Link>
+      <Link to="/social" className="text-cool-gray flex flex-col items-center">
         <Users className="w-6 h-6"/>
         <span className="text-xs mt-1">Social</span>
-      </button>
-      <button className="text-cool-gray flex flex-col items-center">
+      </Link>
+      <Link to="/profile" className="text-cool-gray flex flex-col items-center">
         <User className="w-6 h-6"/>
         <span className="text-xs mt-1">Profile</span>
-      </button>
+      </Link>
     </nav>
   );
 };
