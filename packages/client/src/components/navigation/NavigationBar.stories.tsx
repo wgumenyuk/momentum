2666f243b5d89e66d130c834d280c4fd5b66988e
SnlperStripes <fabiogr@gmx.de>
2024-05-24 10:18:52 +0200

    fix: wrap navigationBar story with a "BrowserRouter" decorator

--- packages/client/src/components/navigation/NavigationBar.stories.tsx
+++ packages/client/src/components/navigation/NavigationBar.stories.tsx
@@ -1,11 +1,20 @@
-// NavigationBar.stories.tsx
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
 
 export default {
   title: "Components/NavigationBar",
-  component: NavigationBar
+  component: NavigationBar,
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story/>
+      </BrowserRouter>
+    )
+  ]
 } as Meta;
 
-export const Default: StoryFn = () => <NavigationBar/>;
+const Template: StoryFn = () => <NavigationBar/>;
+
+export const Default = Template.bind({});
