8a30aa7e31caea04e4458c9be3bed7364740f4c8
SnlperStripes <fabiogr@gmx.de>
2024-05-24 10:00:10 +0200

    feat: add routes to react-router-dom

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -7,13 +7,20 @@ import "./index.css";
 import LoginPage from "./pages/auth/LoginPage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/auth/RegisterPage";
+import { NavigationBar } from "$components/Navigation";
+import WorkoutsPage from "pages/workout/WorkoutsOverview";
+
 
 const root = document.getElementById("root")!;
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage/>
+    element: (
+      <>
+        <HomePage/>
+      </>
+    )
   },
   {
     path: "/login",
@@ -22,6 +29,33 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterPage/>
+  },
+  {
+    path: "/workouts",
+    element: (
+      <>
+        <WorkoutsPage/>
+        <NavigationBar/>
+      </>
+    )
+  },
+  {
+    path: "/social",
+    element: (
+      <>
+        {/* <SocialPage /> - not yet developed*/}
+        <NavigationBar/>
+      </>
+    )
+  },
+  {
+    path: "/profile",
+    element: (
+      <>
+        {/* <ProfilePage /> - not yet developed*/}
+        <NavigationBar/>
+      </>
+    )
   }
 ]);
 
