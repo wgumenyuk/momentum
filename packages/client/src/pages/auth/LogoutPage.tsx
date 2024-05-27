14011389753be3a06e4d8ed9bf1e7c583c0dec5a
Léonard Munch <leonard_munch@hotmail.com>
2024-05-28 01:30:40 +0200

    feat: added tailwind css

--- packages/client/src/pages/auth/LogoutPage.tsx
+++ packages/client/src/pages/auth/LogoutPage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BackgroundLayout } from "$components/Background";
-import "./LogoutPageStyles.css";
 
 const LogoutPage: React.FC = () => {
   const handleLogout = () => {
@@ -16,12 +15,22 @@ const LogoutPage: React.FC = () => {
 
   return (
     <BackgroundLayout>
-      <div className="container">
-        <div className="confirmation-box">
-          <div className="question">Are you sure you want to log out?</div>
-          <div className="options">
-            <div className="option" onClick={handleLogout}>Yes</div>
-            <div className="option" onClick={handleCancel}>No</div>
+      <div className="flex justify-center items-center h-full w-full">
+        <div className="w-90 h-40 bg-[#D4D9E3] rounded-lg flex flex-col items-center justify-between p-5 box-border">
+          <div className="text-[#011518] text-lg text-center mt-2.5">Are you sure you want to log out?</div>
+          <div className="flex justify-between w-full mt-4">
+            <div
+              className="w-32 h-12 bg-gradient-to-r from-[#40A3BC] to-[#245A68] rounded-lg flex justify-center items-center text-white text-lg cursor-pointer transition duration-300 hover:bg-gradient-to-r hover:from-[#245A68] hover:to-[#40A3BC] mx-2"
+              onClick={handleLogout}
+            >
+              Yes
+            </div>
+            <div
+              className="w-32 h-12 bg-gradient-to-r from-[#40A3BC] to-[#245A68] rounded-lg flex justify-center items-center text-white text-lg cursor-pointer transition duration-300 hover:bg-gradient-to-r hover:from-[#245A68] hover:to-[#40A3BC] mx-2"
+              onClick={handleCancel}
+            >
+              No
+            </div>
           </div>
         </div>
       </div>
