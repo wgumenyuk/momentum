46ea5110bc662e4f994b27b49a9407be0fef8d02
SnlperStripes <fabiogr@gmx.de>
2024-06-03 17:15:15 +0200

    fix: adjust color of background for entries

--- packages/client/src/pages/workout/FilterMuscleGroups.tsx
+++ packages/client/src/pages/workout/FilterMuscleGroups.tsx
@@ -26,12 +26,12 @@ export const FilterMuscleGroupsPage: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 p-6">
+    <div className="min-h-screen bg-gray-100 p-6">
       <div className="space-y-4">
-        <h2 className="text-xl font-semibold text-grey-500">Filter muscle groups</h2>
+        <h2 className="text-xl font-semibold text-gray-700">Filter muscle groups</h2>
         {Object.keys(groupSelection).map((group) => (
-          <div key={group} className="flex justify-between items-center bg-gray-800 p-4 rounded-lg shadow-md">
-            <span className="text-lg font-semibold text-grey-500">{group}</span>
+          <div key={group} className="flex justify-between items-center bg-blue-800 p-4 rounded-lg shadow-md">
+            <span className="text-lg font-semibold text-gray-700">{group}</span>
             <Checkbox
               variant="blank"
               checked={groupSelection[group as MuscleGroupsType]}
