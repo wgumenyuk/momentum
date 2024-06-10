439417c5577e8c7ba3737cc28ff971c9367aa01e
wgumenyuk <wlad@gumenyuk.de>
2024-06-10 17:11:34 +0200

    feat: add missing barrel exports to api

--- packages/client/src/internal/api/index.ts
+++ packages/client/src/internal/api/index.ts
@@ -1,1 +1,6 @@
 export * from "$internal/api/auth";
+export * from "$internal/api/exercises";
+export * from "$internal/api/friendships";
+export * from "$internal/api/past-workouts";
+export * from "$internal/api/users";
+export * from "$internal/api/workouts";
