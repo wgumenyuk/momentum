ca98aca227c9b1c9a2230a360071dc93b6764570
wgumenyuk <wlad@gumenyuk.de>
2024-06-17 21:56:19 +0200

    feat: add `isAuthenticated` middleware to events router

--- packages/server/src/api/v1/events.ts
+++ packages/server/src/api/v1/events.ts
@@ -1,11 +1,14 @@
 import Router from "@koa/router";
 
 // Intern
+import { isAuthenticated } from "$api/middleware/auth";
 import { getEvents, getEvent } from "$services/events";
 
 export const eventsRouter = new Router({
   prefix: "/events"
 });
+
+eventsRouter.use(isAuthenticated);
 
 eventsRouter.get("/", async (ctx) => {
   await getEvents(ctx);
