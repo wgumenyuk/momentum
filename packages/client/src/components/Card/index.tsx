86d3d5ae3658aee86e52e8aa273ce5e4829a6653
wgumenyuk <wlad@gumenyuk.de>
2024-06-18 14:12:33 +0200

    feat: accept optional class names in `Card` component

--- packages/client/src/components/Card/index.tsx
+++ packages/client/src/components/Card/index.tsx
@@ -1,15 +1,19 @@
+import clsx from "clsx";
+
+// Types
 import type { FC, ReactNode } from "react";
 
 type CardProps = {
+  className?: string;
   children: ReactNode;
 };
 
 /**
   `Card`-Komponente.
 */
-export const Card: FC<CardProps> = ({ children }) => {
+export const Card: FC<CardProps> = ({ className, children }) => {
   return (
-    <div className="bg-blue-800 p-4 rounded-xl">
+    <div className={clsx("bg-blue-800 p-4 rounded-xl", className)}>
       {children}
     </div>
   );
