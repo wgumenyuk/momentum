089702ffea85044cacc4a75d7997189329bf69fa
wgumenyuk <wlad@gumenyuk.de>
2024-06-20 00:02:11 +0200

    feat: add events router to api

--- packages/server/src/api/v1/index.ts
+++ packages/server/src/api/v1/index.ts
@@ -7,7 +7,8 @@ import { authRouter } from "$api/v1/auth";
 import { usersRouter } from "$api/v1/users";
 import { workoutRouter } from "$api/v1/workouts";
 import { pastWorkoutRouter } from "$api/v1/past-workouts";
-import { friendshipRouter } from "./friendship";
+import { friendshipRouter } from "$api/v1/friendship";
+import { eventsRouter } from "$api/v1/events";
 
 /**
   Router.
@@ -21,6 +22,7 @@ router.use(usersRouter.routes());
 router.use(workoutRouter.routes());
 router.use(pastWorkoutRouter.routes());
 router.use(friendshipRouter.routes());
+router.use(eventsRouter.routes());
 
 router.get("/status", (ctx) => {
   ok(ctx, StatusCode.Success);
