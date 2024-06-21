af3a4a1caa424dbb9e79d8cb18cac73050b5ebf4
wgumenyuk <wlad@gumenyuk.de>
2024-06-21 11:02:02 +0200

    feat: add display name input to `Profile` page

--- packages/client/src/pages/ProfilePage.tsx
+++ packages/client/src/pages/ProfilePage.tsx
@@ -1,17 +1,88 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { LogOutIcon } from "lucide-react";
+import {
+  LogOutIcon,
+  UserIcon,
+  CheckIcon
+} from "lucide-react";
 
 // Intern
 import { User } from "$internal/api";
 import { useJwt } from "$components/JwtContext";
 import { BackgroundLayout } from "$components/Background";
 import { Card } from "$components/Card";
+import { Input } from "$components/Input";
 import { Switch } from "$components/Switch";
 import { Button } from "$components/Button";
 
 // Types
 import type { FC } from "react";
+
+type DisplayNameProps = {
+  displayName: string;
+};
+
+const DisplayName: FC<DisplayNameProps> = ({
+  displayName: _displayName
+}) => {
+  const [ displayName, setDisplayName ] = useState(_displayName);
+  const [ showSuccess, setShowSuccess ] = useState(false);
+  const [ error, setError ] = useState(false);
+
+  const updateDisplayName = async () => {
+    const response = await User.updateDisplayName(displayName);
+
+    if(!response || !response.ok) {
+      setError(true);
+      return;
+    }
+
+    setShowSuccess(true);
+    setError(false);
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1500);
+    });
+
+    setShowSuccess(false);
+  };
+
+  return (
+    <Card>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-0.5">
+          <span className="font-bold">Display name</span>
+          <span className="text-sm">
+            This name will be seen publicly.
+          </span>
+        </div>
+        <Input
+          type="text"
+          className="bg-blue-700"
+          icon={UserIcon}
+          value={displayName}
+          onChange={(value) => setDisplayName(value)}
+        />
+        {error && (
+          <span className="text-red-400 text-sm">
+            Invalid display name. 
+          </span>
+        )}
+        <Button
+          variant="blue"
+          onClick={updateDisplayName}
+        >
+          <span className="flex justify-center items-center gap-2">
+            Save
+            {showSuccess && (
+              <CheckIcon size="24px" className="text-green-300"/>
+            )}
+          </span>
+        </Button>
+      </div>
+    </Card>
+  );
+};
 
 export const ProfilePage: FC = () => {
   const [ displayName, setDisplayName ] = useState("");
@@ -87,6 +158,8 @@ export const ProfilePage: FC = () => {
                   <div className="w-16 h-16 bg-gray rounded-lg"/>
                 </div> 
               </Card>
+
+              <DisplayName displayName={displayName}/>
 
               <Card>
                 <div className="flex justify-between items-center gap-4">
