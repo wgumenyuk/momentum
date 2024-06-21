d1054529be4e8a66a389d8497c0fc443719d2094
wgumenyuk <wlad@gumenyuk.de>
2024-06-21 10:13:59 +0200

    feat: add api route for updating display names

--- packages/server/src/api/v1/users.ts
+++ packages/server/src/api/v1/users.ts
@@ -2,7 +2,11 @@ import Router from "@koa/router";
 
 // Intern
 import { isAuthenticated } from "$api/middleware/auth";
-import { getUser, deleteUser } from "$services/user";
+import {
+  getUser,
+  updateDisplayName,
+  deleteUser
+} from "$services/user";
 
 /**
   Nutzer-Router.
@@ -15,6 +19,10 @@ usersRouter.use(isAuthenticated);
 
 usersRouter.get("/:uid", async (ctx) => {
   await getUser(ctx);
+});
+
+usersRouter.put("/display-name", async (ctx) => {
+  await updateDisplayName(ctx);
 });
 
 usersRouter.delete("/", async (ctx) => {
