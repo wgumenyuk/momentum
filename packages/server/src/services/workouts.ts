19337b7d8d0d5edda1131f9b36ef6d2638f4d62d
SnlperStripes <fabiogr@gmx.de>
2024-06-28 17:30:58 +0200

    refactor: remove old debugging code

--- packages/server/src/services/workouts.ts
+++ packages/server/src/services/workouts.ts
@@ -12,7 +12,6 @@ import { Workout } from "$models/workout";
 // Types
 import type { Context } from "koa";
 import type { ErrorCodeValue } from "@momentum/shared";
-import { log } from "$internal/logger";
 
 /**
   Erstellt einen neues Workout.
@@ -67,13 +66,11 @@ export const getWorkout = async (ctx: Context) => {
   Ruft alle Workouts eines Nutzers ab.
 */
 export const getWorkouts = async (ctx: Context) => {
-  log.debug("get workout called");
   const userId = ctx.state.user.id;
 
   const workouts = await Workout.find({
     userId
   }, "-_id -__v");
-  log.debug(workouts);
 
   ok(ctx, StatusCode.Success, {
     workouts
