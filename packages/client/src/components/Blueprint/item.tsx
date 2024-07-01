bb1d2bb8b8a6ddd78d2a37f979df3c035f43eeb3
Léonard Munch <leonard_munch@hotmail.com>
2024-07-01 17:18:50 +0200

    feat: removed unnecessary files and additional design tweak in item.tsx and list.tsx for blueprint

--- packages/client/src/components/Blueprint/item.tsx
+++ packages/client/src/components/Blueprint/item.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Edit } from "lucide-react";
 
 export interface BlueprintItemProps {
   name: string;
@@ -13,7 +14,7 @@ export const BlueprintItem: React.FC<BlueprintItemProps> = ({ name, description 
         <p className="text-sm text-gray-600">{description}</p>
       </div>
       <button className="p-2 bg-white rounded-lg shadow-md">
-        {/* Add action buttons here */}
+        <Edit className="w-7 h-7 text-current"/>
       </button>
     </div>
   );
