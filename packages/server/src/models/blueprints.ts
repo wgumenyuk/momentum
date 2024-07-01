36101b9bd7f517b2acdccecf5a3959631f1c0e59
Léonard Munch <leonard_munch@hotmail.com>
2024-07-01 16:19:45 +0200

    feat: added model file for blueprints

---
+++ packages/server/src/models/blueprints.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+// Types
+import type { Blueprint as BlueprintType } from "@momentum/shared";
+
+const BlueprintSchema = new Schema<BlueprintType>({
+  id: {
+    type: "string",
+    required: true,
+    unique: true
+  },
+  userId: {
+    type: "string",
+    required: true
+  },
+  name: {
+    type: "string",
+    required: true
+  },
+  description: {
+    type: "string"
+  },
+  exercises: {
+    type: [
+      {
+        exerciseId: "string",
+        sets: "number",
+        reps: "number"
+      }
+    ]
+  }
+});
+
+/**
+  `Blueprint`-Modell.
+*/
+export const Blueprint = model<BlueprintType>("Blueprint", BlueprintSchema);
